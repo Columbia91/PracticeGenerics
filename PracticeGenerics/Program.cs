5aa316d616b96197b4a9f58991d33a3ad4c97cb5
Columbia91 <alpha.z91@mail.ru>
2019-03-06 21:41:19 +0600

    Добавьте файлы проекта.

---
+++ PracticeGenerics/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace PracticeGenerics
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string text;
+            using(StreamReader sr = new StreamReader("text.txt"))
+            {
+                text = sr.ReadToEnd();
+                text = text.Trim(new char[] { '\n' });
+            }
+            
+            string[] words = text.Split(new char[] { ' ', ',', '.', '\n' }, StringSplitOptions.RemoveEmptyEntries);
+
+            foreach (var word in words)
+            {
+                Console.WriteLine(word);
+            }
+
+            Dictionary<int, string> dictionary = new Dictionary<int, string>();
+            for (int i = 0; i < words.Length; i++)
+            {
+                dictionary.Add(i, words[i]);
+            }
+            
+            int count = 0;
+            Dictionary<int, string> newDictionary = new Dictionary<int, string>();
+
+            for (int i = 0; i < dictionary.Count; i++)
+            {
+                if (!newDictionary.ContainsValue(dictionary[i]))
+                {
+
+                }
+            }
+        }
+    }
+}
