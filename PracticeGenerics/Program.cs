6f147fe2512069c25f19610355758be500b271b8
cougar91 <alpha.z91@mail.ru>
2019-03-07 11:25:40 +0600

    2nd change

--- PracticeGenerics/Program.cs
+++ PracticeGenerics/Program.cs
@@ -16,16 +16,10 @@ namespace PracticeGenerics
             using (StreamReader sr = new StreamReader("text.txt"))
             {
                 text = sr.ReadToEnd();
-                text = text.Trim(new char[] { '\n' });
             }
             
-            string[] words = text.Split(new char[] { ' ', ',', '.' }, StringSplitOptions.RemoveEmptyEntries);
-
-            foreach (var word in words)
-            {
-                Console.WriteLine(word);
-            }
-
+            string[] words = text.Split(new string[] { " ", ",", ".", "\n" }, StringSplitOptions.RemoveEmptyEntries);
+            
             Dictionary<int, string> dictionary = new Dictionary<int, string>();
             for (int i = 0; i < words.Length; i++)
             {
@@ -43,10 +37,21 @@ namespace PracticeGenerics
                 }
             }
 
+            int count = 0;
+            Console.WriteLine("{0, 23} {1,22}", "Слово", "Кол-во");
             for (int i = 0; i < newDictionary.Count; i++)
             {
-                Console.WriteLine(newDictionary[i]);
+                if (newDictionary[i] == " ")
+                    continue;
+                for (int j = 0; j < dictionary.Count; j++)
+                {
+                    if (newDictionary[i] == dictionary[j])
+                        count++;
+                }
+                Console.WriteLine($"{i+1, 2} {newDictionary[i], 20} {count, 20}");
+                count = 0;
             }
+            Console.WriteLine($"Всего слов: {dictionary.Count}, из них уникальных: {newDictionary.Count}");
         }
     }
 }
